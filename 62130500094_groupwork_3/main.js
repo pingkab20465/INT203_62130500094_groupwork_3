6a00fb67f6daf5a7595e7402063147f0c895519b
Sittichok Pintakul <sittichok.ping@mail.kmutt.ac.th>
2021-02-10 12:10:51 +0700

    [Update] First Production

--- 62130500094_groupwork_3/main.js
+++ 62130500094_groupwork_3/main.js
@@ -1,9 +1,30 @@
 const app = {
     data() {
-        return {
-            msg: 'Hello, Vue3'
+        return { 
+            pics: [
+                ['./images/fox.jpg', 'Fox', false]
+                ,['./images/cloud.jpg', 'Cloud', false]
+                ,['./images/bird.jpg', 'Bird', false]
+                // ,['./images/others/man.jpg', 'Man', false]
+                // ,['./images/others/forest.jpg', 'Forest', false]
+                // ,['./images/others/reeds.jpg', 'Reeds', false]
+            ]
+        }
+    },
+    computed: {
+        countPics() {
+            return this.pics.length;
+        },
+        totalLikes() {
+            return this.pics.filter(i => i[2]).length
+        }
+    },
+    methods: {
+        toggleLike(pic) {
+            console.log(pic);
+            pic[2]= !pic[2];
         }
     }
 
 }
-Vue.createApp(app).mount('#app')
+mountedApp = Vue.createApp(app).mount('#app');
